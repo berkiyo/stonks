2980f53c01fca83a2a333bbd01c892bca6952e27
berkiyo <berk@tekbyte.net>
2021-01-17 18:02:27 +1100

    Added project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # stonks
 A fancy compound interest calculator for Android. 
+
+Currently work-in-progress.
---
+++ app/src/main/java/com/berkd/stonks/MainActivity.java
@@ -0,0 +1,19 @@
+package com.berkd.stonks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    /**
+     * TODO: Implement the basic user interface for calculating compound interest
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
